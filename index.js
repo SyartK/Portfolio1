bdf5d8566e3aa2784b86c789a6edeec64e3b151d
SyartK <114510564+SyartK@users.noreply.github.com>
2026-05-14 16:45:20 +0200

    Update portfolio design and projects

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple", {
-  strings: ["Web Developer", "Front-End Developer"],
+  strings: ["Full-Stack Engineer", "Laravel Developer", "PHP Developer"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
